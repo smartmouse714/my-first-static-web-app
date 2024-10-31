95e6e3af6a0cd3620fdebbe6f7d0f8b4c1b503ef
奇异士 <8274811+smartmouse714@users.noreply.github.com>
2024-10-31 18:03:50 +1100

    Add hints to selected cities.

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,5 +1,5 @@
-// Define a function to get the current time in different cities
-function getCityTimes() {
+// Define a function to update the clock display
+function updateTime() {
     const tzList = [
         "Pacific/Auckland",
         "Europe/London",
@@ -12,32 +12,23 @@ function getCityTimes() {
         "America/Denver"
     ];
 
-    const cityTimes = {};
-    const timeInTimeZone = new Date();
-    tzList.forEach(toTimeZone => {
-        const city = toTimeZone.split('/')[1];
-        cityTimes[city] = timeInTimeZone.toLocaleTimeString('en-US', { timeZone: toTimeZone, hour: '2-digit', minute: '2-digit' });
-    });
-
-    return cityTimes;
-}
-
-// Define a function to update the clock display
-function updateTime() {
-    const cityTimes = getCityTimes();
-    // Update clock display for each city
-    Object.keys(cityTimes).forEach(city => {
+    const timeObj = new Date();
+    tzList.forEach(tz => {
+        const city = tz.split('/')[1];
         const clockElement = document.getElementById(`${city}Clock`);
         if (clockElement) {
-            clockElement.textContent = cityTimes[city];
+            clockElement.textContent = timeObj.toLocaleTimeString('en-US', {
+                timeZone: tz,
+                // hour: '2-digit', minute: '2-digit',
+            });
         }
+
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const dateCaptionElement = document.getElementById("currentDate");
     const today = new Date();
-    const formattedDate = today.toISOString().split('T')[0];
-    dateCaptionElement.textContent = formattedDate;
+    dateCaptionElement.textContent = today.toISOString().split('T')[0];
 });
-setInterval(updateTime, 1000*10); // Update every 10 seconds
+setInterval(updateTime, 1000); // Update every second
